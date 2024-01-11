f2804da231970a8e561da598847f4ee8fbd8b18f
AlolaSunday12 <alolasj@gmail>
2024-01-11 14:26:31 +0000

    python variable

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module provides a function 'add' that takes two floats and returns their sum.
+
+Author: Your Name
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum.
+
+    :param a: The first float number.
+    :param b: The second float number.
+    :return: The sum of 'a' and 'b' as a float.
+    """
+    return a + b
